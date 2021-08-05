07aa5ca587380a9ca439c6dcbdb8dc1579fdddaa
saif2github <53149396+saif2github@users.noreply.github.com>
2021-08-05 12:15:32 +0530

    Update and rename insert.php to db_function.php

---
+++ db_function.php
@@ -0,0 +1,21 @@
+<?php
+function insert($conn,  $data, $table)
+{
+    foreach ($data as $key => $value) {
+        $array[] = "`$key`='" . mysqli_real_escape_string($conn, $value) . "'";
+    }
+    $datatoupdate = implode(", ", $array);
+    $s = "INSERT INTO  `$table` SET $datatoupdate";
+    $result =  mysqli_query($conn, $s) || die("update Query failed ------------> " . mysqli_error($conn));
+    return $result ? true : false;
+}
+function update($conn,  $data, $table,$id)
+{
+    foreach ($data as $key => $value) {
+        $array[] = "`$key`='" . mysqli_real_escape_string($conn, $value) . "'";
+    }
+    $datatoupdate = implode(", ", $array);
+    $s = "UPDATE  `$table` SET $datatoupdate WHERE id = $id";
+    $result =  mysqli_query($conn, $s) || die("update Query failed ------------> " . mysqli_error($conn));
+    return $result ? true : false;
+}
