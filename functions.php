2c8e9ff17b1957354ed460efef1b6762c1b783f8
saif2github <53149396+saif2github@users.noreply.github.com>
2021-08-09 12:23:28 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,8 +10,15 @@ function in_array_r($needle, $haystack, $strict = false)
 } 
 
 //file upload 
-  $filesArray = ["cv"];
   $dir = "images/";
+  $filesArray = [
+    "image1",
+    "image2",
+    "image3",
+    "image4",
+    "image5",
+    "image6",
+  ];
   foreach ($filesArray as $item) {
     if (isset($_FILES[$item]['name']) && ($_FILES[$item]['name'] != null)) {
       ($_POST[$item] =  time() . "-" . $_FILES[$item]['name']);
@@ -19,3 +26,24 @@ function in_array_r($needle, $haystack, $strict = false)
     }
   }
 ?>
+
+//file upload update
+  $dir = "images/";
+  $filesArrayUpdate = [
+    "image1",
+    "image2",
+    "image3",
+    "image4",
+    "image5",
+    "image6",
+  ];
+  $filesArray = array();
+  foreach ($filesArrayUpdate as $itemArr) {
+    isset($_FILES[$itemArr]['name']) ? array_push($filesArray, $itemArr) : null;
+  }
+  foreach ($filesArray as $item) {
+    if (isset($_FILES[$item]['name']) && ($_FILES[$item]['name'] != null)) {
+      ($_POST[$item] =  time() . "-" . $_FILES[$item]['name']);
+      move_uploaded_file($_FILES[$item]['tmp_name'], ($dir . $_POST[$item]));
+    }
+  }
